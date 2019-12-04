63028dd033bf459efaf20f5c4a381d4269354790
fady0bassem <fady0bassem@gmail.com>
2019-12-05 00:07:26 +0200

    commit

--- app/src/main/java/com/fadybassem/bittaskjava/ui/adapters/HomeAdapter.java
+++ app/src/main/java/com/fadybassem/bittaskjava/ui/adapters/HomeAdapter.java
@@ -16,6 +16,8 @@ import com.fadybassem.bittaskjava.R;
 import com.fadybassem.bittaskjava.data.model.pojo.HomeModel;
 import com.fadybassem.bittaskjava.utils.RoundedCornersTransform;
 import com.fadybassem.bittaskjava.utils.TouchImageView;
+import com.github.loadingview.LoadingView;
+import com.squareup.picasso.Callback;
 import com.squareup.picasso.Picasso;
 
 import java.util.List;
@@ -43,14 +45,25 @@ public class HomeAdapter extends RecyclerView.Adapter<HomeAdapter.ViewHolder> {
 
         if (datum != null) {
             if (datum.getImage() != null) {
+                holder.loadingview.start();
 
                 Picasso.get().load(datum.getImage())
                         .transform(new RoundedCornersTransform())
                         .error(android.R.drawable.ic_menu_report_image)
-                        .into(holder.imageview);
+                        .into(holder.imageview, new Callback() {
+                            @Override
+                            public void onSuccess() {
+                                holder.loadingview.setVisibility(View.GONE);
+                            }
+
+                            @Override
+                            public void onError(Exception e) {
+                                holder.loadingview.setVisibility(View.GONE);
+                            }
+                        });
 
                 holder.itemView.setOnClickListener(v -> {
-                    LayoutInflater inflater = (LayoutInflater) context.getSystemService( Context.LAYOUT_INFLATER_SERVICE );
+                    LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
                     View imageDialog = inflater.inflate(R.layout.dialog_image, null);
                     Dialog dialog = new Dialog(context, android.R.style.Theme_Black_NoTitleBar_Fullscreen);
                     dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
@@ -59,10 +72,20 @@ public class HomeAdapter extends RecyclerView.Adapter<HomeAdapter.ViewHolder> {
                     dialog.getWindow().setBackgroundDrawable(new ColorDrawable(android.graphics.Color.TRANSPARENT));
 
                     TouchImageView imgImage = imageDialog.findViewById(R.id.imgImage);
-
+                    LoadingView loadingview = imageDialog.findViewById(R.id.loadingview);
+                    loadingview.start();
                     Picasso.get().load(datumList.get(position).getImage())
                             .error(android.R.drawable.ic_menu_report_image)
-                            .into(imgImage);
+                            .into(imgImage, new Callback() {
+                                @Override
+                                public void onSuccess() {loadingview.setVisibility(View.GONE);
+                                }
+
+                                @Override
+                                public void onError(Exception e) {
+                                    loadingview.setVisibility(View.GONE);
+                                }
+                            });
 
                     dialog.show();
 
@@ -80,10 +103,12 @@ public class HomeAdapter extends RecyclerView.Adapter<HomeAdapter.ViewHolder> {
 
     public class ViewHolder extends RecyclerView.ViewHolder {
         ImageView imageview;
+        LoadingView loadingview;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
             imageview = itemView.findViewById(R.id.imageview);
+            loadingview = itemView.findViewById(R.id.loadingview);
         }
     }
 
